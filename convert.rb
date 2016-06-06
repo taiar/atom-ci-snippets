f708c9ad511804a26f50591ab0b6ab9f0574a8d0
André Taiar <andre.taiar@appprova.com.br>
2016-06-06 18:21:07 -0300

    Fixing line ending on snippets.

--- convert.rb
+++ convert.rb
@@ -9,6 +9,7 @@ def txt_or_empty(obj)
   obj.nil? ? '' : obj.text
                      .gsub(/['"\\\x0]/, '\\\\\0')
                      .gsub(/^\n/, '')
+                     .gsub(/\n$/, '')
                      .gsub(/\n/, '\n')
 end
 
@@ -26,4 +27,4 @@ Dir.glob('./ci-snippets/*.sublime-snippet') do |item|
   snippet << "    'body': '#{content}'\n"
 end
 
-File.open('atom-ci-snippets', 'w+') { |file| file.write snippet }
+File.open('./snippets/atom-ci-snippets.cson', 'w+') { |file| file.write snippet }
